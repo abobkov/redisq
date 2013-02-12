8348fe8d189ded8da87f7c8a68bd377379a89433
Alexander Bobkov <abobkov@swdrom.com>
2013-02-12 11:33:12 +0400

    add point 2

--- frontend/controllers/index.js
+++ frontend/controllers/index.js
@@ -2,7 +2,36 @@
 var redisq = require('../../');
 
 module.exports.index = function(req, res) {
-	redisq.getStats(function(err, stats) {
-		res.render("index", { stats: stats });
-	});
+    var qstats = [],
+        counters = [];
+
+    redisq.getQueues(function(err, queues) {
+        for (elem in queues) {
+            var stats = new Stats(elem)
+            counters.push([stats, function(err, counters, qname){
+                        qstats.push({ "qname": qname, "counters": counters })
+                }]
+            )
+        }
+
+        redisq.multiCountersGet(counters, function(){
+            res.render("index", { stats: qstats });
+        })
+    });
+
+    redisq.multiCountersGet = function(counters, cb) {
+        var callCount = counters.length;
+
+        counters.forEach(function(stats_fn){
+            stats_fn[0].countersGet.call(stats_fn[0], function(err, counters, qname){
+                stats_fn[1].apply(stats_fn[0], arguments);
+
+                callCount--;
+                if(callCount == 0){
+                    cb();
+                }
+            })
+        })
+    }
 };
+
