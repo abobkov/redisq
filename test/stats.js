908bd03abc55165b3e560e2ac9914c4543a66487
Alexander Bobkov <abobkov@swdrom.com>
2013-02-12 19:55:11 +0400

    optimize _recalculateHistory

---
+++ test/stats.js
@@ -0,0 +1,10 @@
+var async = require("async"),
+    redis  = require("../lib/redis"),
+    Stats = require("../lib/stats");;
+
+describe("/stats", function() {
+
+    describe("#recalculateHistory", function() {
+
+    });
+});
